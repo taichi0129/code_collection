f45e9e86988ec0099131ddf8490e369fdbd66051
taichi0129 <nishi.made.t.9210@gmail.com>
2020-08-14 19:26:23 +0900

    js-study

--- javaScript/javascript_practice/slide_image/javascript2/slide_image.js
+++ javaScript/javascript_practice/slide_image/javascript2/slide_image.js
@@ -1,82 +1,79 @@
 window.onload = function() {
-  /*=======================
+  
+  /*============================
   * 変数の定義
-  *======================*/
-
-  // 画像のリストの定義
-  var photoList = [
-    { src: 'img/spring.jpg', title: '春の桜' },
-    { src: 'img/summer.jpg', title: '夏のひまわり' },
-    { src: 'img/autumn.jpg', title: '秋の紅葉' },
-    { src: 'img/winter.jpg', title: '冬の山' }
+  *===========================*/
+ 
+ // 画像リストの定義
+ var photoList = [
+   { src: 'img/spring.jpg', title: '春の桜' },
+   { src: 'img/summer.jpg', title: '夏のひまわり' },
+   { src: 'img/autumn.jpg', title: '秋の紅葉' },
+   { src: 'img/winter.jpg', title: '冬の山' }
   ];
-
   var photoLength = photoList.length; //=> 4
-
+  
   // 要素の取得
   var photo = document.getElementById('photo');
   var nextBtn = document.getElementById('nextBtn');
-  var title = document.getElementById('title')
-
+  var title = document.getElementById('title');
+  
   // 現在のインデックスを保存するための変数
   var currentIndex = 0;
 
-  /*========================
-  * 関数の定義
-  *=======================*/
-
+  /*============================
+   * 関数の定義
+   *===========================*/
+  
   // 指定の画像に表示を切り替える関数
-  function showPhoto() {
+  function showPhoto(index) {
     // 全ての画像を非表示
-    for(var i = 0; i < photoLength; i++) {
+    for (var i = 0; i < photoLength; i++) {
       photoList[i].elem.style.display = 'none';
     }
-
+  
     // 表示する対象の要素を取得
     var targetPhoto = photoList[index];
-
+  
     // タイトルの表示
     var viewNumber = index + 1;
     title.innerHTML = '[' + viewNumber + ']' + targetPhoto.title;
-
+  
     // 画像の表示
-    targetPhoto.elem,style.display = 'inline';
-  };
+    targetPhoto.elem.style.display = 'inline';
+  }
 
-  /*=========================
-  * イベントの設定
-  *========================*/
+  /*=============================
+   * イベントの設定
+   *============================*/
 
   // nextボタンを押した時の処理
   nextBtn.onclick = function() {
-    // 表示する画像のインデックスを計算
+    // 表示するためのインデックスを計算
     currentIndex++;
-    if(currentIndex === photoLength) {
+    if (currentIndex === photoLength) {
       currentIndex = 0;
     }
-    
+
     // 画像の切り替え
     showPhoto(currentIndex);
   };
 
-
-
-  /*========================
-  * 初期化処理
-  *=======================*/
+  /*=============================
+   * 初期化処理
+   *============================*/
 
   // img要素をHTMLに追加
   var item, img;
-  for(var i = 0; i < photoLength; i++) {
+  for (var i = 0; i < photoLength; i++) {
     item = photoList[i];
 
     // img要素の作成
     img = document.createElement('img');
 
-    // 作成したimg要素に属性を設定
+    // 作成したimg要素に属性を追加
     img.src = item.src;
     img.alt = item.title;
-    img.style.display = 'none';
 
     // 作成したimg要素をHTMLに追加
     photo.appendChild(img);
@@ -87,4 +84,4 @@ window.onload = function() {
 
   // 初期表示のためにshowPhoto関数を実行する
   showPhoto(currentIndex);
-};
+}
